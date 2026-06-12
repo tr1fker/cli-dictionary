5a478ea34c7d1c88f42e4ff70bd65f600257602b
tr1fker <cfjhxhkxck@gmail.com>
2026-06-12 13:01:57 +0300

    Revert "Merge pull request #1 from tr1fker/feature/postgresql-conn"
    This reverts commit 0b0a17bba2460a1cb4da4c7b4e2d13a1528560fd, reversing
    changes made to 41eada7bc53d962cf36d0967fa0086a51c602b47.

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -1,18 +1,17 @@
 package tr1fker;
 
-import tr1fker.connections.DatabaseConnection;
-
-import java.sql.Connection;
-
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        Connection postgreSQLConnection = DatabaseConnection.getConnection();
+        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
+        // to see how IntelliJ IDEA suggests fixing it.
+        System.out.printf("Hello and welcome!");
 
-        if (postgreSQLConnection == null) {
-            System.out.println("Проблема подключения к PostgreSQL!");
-            return;
+        for (int i = 1; i <= 5; i++) {
+            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
+            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
+            System.out.println("i = " + i);
         }
-
-        System.out.println("Успешное подключение к БД!");
     }
 }
