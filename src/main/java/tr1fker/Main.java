221edf2f21f66a66a956f70327df7231e676decc
tr1fker <cfjhxhkxck@gmail.com>
2026-06-12 12:56:18 +0300

    Added postgresql dependency, successful connection

--- src/main/java/tr1fker/Main.java
+++ src/main/java/tr1fker/Main.java
@@ -1,7 +1,18 @@
 package tr1fker;
 
+import tr1fker.connections.DatabaseConnection;
+
+import java.sql.Connection;
+
 public class Main {
     public static void main(String[] args) {
+        Connection postgreSQLConnection = DatabaseConnection.getConnection();
 
+        if (postgreSQLConnection == null) {
+            System.out.println("Проблема подключения к PostgreSQL!");
+            return;
+        }
+
+        System.out.println("Успешное подключение к БД!");
     }
 }
